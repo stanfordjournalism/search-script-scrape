f0b5491cde153a97bc4e633fb08b3fb607936be8
Dan Nguyen <dansonguyen@gmail.com>
2015-08-12 22:03:30 -0700

    some twitter info

--- scripts/90.py
+++ scripts/90.py
@@ -5,8 +5,15 @@ import json
 import os
 import requests
 import tweepy
-
-
+# You need to have a Twitter account and register as a developer:
+# http://www.compjour.org/tutorials/getting-started-with-tweepy/
+# Your credentials JSON file should look like this:
+# {
+#     "access_token": "AAAA",
+#     "access_token_secret": "BBBB",
+#     "consumer_secret": "CCCC",
+#     "consumer_key": "DDDDD"
+# }
 # Twitter helper methods
 DEFAULT_TWITTER_CREDS_PATH = '~/.creds/me.json' # put your own path here
 def get_api(credsfile = DEFAULT_TWITTER_CREDS_PATH):
@@ -31,22 +38,17 @@ def get_api(credsfile = DEFAULT_TWITTER_CREDS_PATH):
     # create an API handler
     return tweepy.API(auth)
 
-
-
 # gets a whole bunch of profile information from a batch of screen_names
+BATCH_SIZE = 100
 def get_profiles_from_screen_names(snames):
     api = get_api()
-    batch_size = 100
     profiles = []
-    for i in range(ceil(len(snames) / batch_size)):
-        s = i * batch_size
-        bnames = snames[s:(s + batch_size)]
+    for i in range(ceil(len(snames) / BATCH_SIZE)):
+        s = i * BATCH_SIZE
+        bnames = snames[s:(s + BATCH_SIZE)]
         for user in api.lookup_users(screen_names = bnames):
             profiles.append(user._json)
     return profiles
-
-
-
 # Step 1.
 # Basically, you have to rejigger 18.py:
 # (The number of U.S. congressmembers who have Twitter accounts, according to Sunlight Foundation data)
@@ -56,15 +58,12 @@ rows = csv.DictReader(requests.get(csvurl).text.splitlines())
 # note that spreadsheet includes non-sitting legislators, thus the use
 # of 'in_office' attribute to filter
 legislators = [r for r in rows if r['twitter_id'] and r['in_office'] == '1']
-
 # now call twitter
 twitter_profiles = get_profiles_from_screen_names([x['twitter_id'] for x in legislators])
-
 # match up legislators with profiles:
 for lx in legislators:
     ta = [t for t in twitter_profiles if lx['twitter_id'].lower() == t['screen_name'].lower()]
     lx['twitter_profile'] = ta[0] if ta else None
-
 
 def fooey(x):
     t = x['twitter_profile']
