0a1ddc4db99fc427c3aae09826ca06d8fa62c0ee
Dan Nguyen <dansonguyen@gmail.com>
2015-08-13 08:38:32 -0700

    removed dumb temp steps

--- scripts/83.py
+++ scripts/83.py
@@ -1,22 +1,12 @@
 # The sum of White House staffermember salaries in 2014
-# Note: this is a pretty verbose step-by-step
 import requests
 import csv
 url = "https://open.whitehouse.gov/api/views/i9g8-9web/rows.csv?accessType=DOWNLOAD"
 txt = requests.get(url).text
-# save the file temporarily
-f = open("/tmp/salaries2014.csv", "w")
-f.write(txt)
-f.close()
-###
-# reopen (yes, this is wasteful, but whatever)
-f = open("/tmp/salaries2014.csv", "r")
-rows = list(csv.DictReader(f))
 totes = 0
-for r in rows:
+for r in csv.DictReader(txt.splitlines()):
     # remove $ sign, convert to float
     salval = float(r['Salary'].replace('$', ''))
     totes += salval
-
 print(totes)
-
+# 37776925.0
