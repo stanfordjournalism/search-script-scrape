bebce76f5f89d389a45c8521a7f022a4cd23c177
Dan Nguyen <dansonguyen@gmail.com>
2015-08-12 19:58:06 -0700

    task

---
+++ generate_readme.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This script reads from the Google Doc of tasks and generates
+the titles of the tasks, linked to the appropriate file, and this
+(Markdown) text can be pasted into the README.md file
+"""
+import csv
+import requests
+from os.path import exists
+GDOC_URL = 'https://docs.google.com/spreadsheets/d/1JbY_-g9MkGH78Rta0PnE6D8rG8T-wdKGsMa3kAC3bDs/export?format=csv&gid=0'
+
+txt = requests.get(GDOC_URL).text
+tasks = csv.DictReader(txt.splitlines())
+print("""
+| Title  |  Line count |
+|--------|-------------|
+""")
+for row in sorted(tasks, key = lambda r: int(r['Problem No.'])):
+    task = {'num': row['Problem No.'],
+            'title': row['Title']}
+    task['path'] = "scripts/%s.py" % task['num']
+    if exists(task['path']):
+        task['lines'] = len(open(task['path'], encoding = 'utf-8').readlines())
+    else:
+        task['lines'] = "Not in repo."
+
+    print("| {num}. [{title}]({path})  |  {lines} |".format(**task))
