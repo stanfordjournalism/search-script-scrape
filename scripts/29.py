a1d9047d66de83bdc4829702c8b04793ca454104
Dan Nguyen <dansonguyen@gmail.com>
2015-08-13 08:49:36 -0700

    whoops, TX executioner moves faster than its web developer

--- scripts/29.py
+++ scripts/29.py
@@ -3,12 +3,11 @@
 from datetime import datetime
 from lxml import html
 import requests
-# get next execution
 url = "http://www.tdcj.state.tx.us/death_row/dr_scheduled_executions.html"
 doc = html.fromstring(requests.get(url).text)
-# sometimes the executions happen more quickly
-# than they can update the page so we can't assume the first row
-# is always the upcoming execution
+# sometimes the executions happen more quickly than they can
+# update the scheduled executions list, so we can't assume the
+# first row is always the upcoming execution
 today =  datetime.now()
 for row in doc.xpath('//table/tr')[1:]:
     col = row.cssselect('td')[0]
@@ -16,5 +15,5 @@ for row in doc.xpath('//table/tr')[1:]:
     # meh, whatever, too lazy to look up timezone information
     if exdate.strftime('%Y-%m-%d') >= today.strftime('%Y-%m-%d'):
         # if there's an execution today, the result will be -1
-        print((exdate - datetime.now()).days)
+        print((exdate - today).days)
         break
