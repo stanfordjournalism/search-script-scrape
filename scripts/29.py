149ae8e5af20dea59e6d662bb4022a067163e7da
Dan Nguyen <dansonguyen@gmail.com>
2015-08-13 18:36:43 -0700

    Um, I think I made it time-zone aware?

--- scripts/29.py
+++ scripts/29.py
@@ -1,17 +1,24 @@
 # Number of days until Texas's next scheduled execution
 from datetime import datetime
 from lxml import html
+import pytz
 import requests
 url = "http://www.tdcj.state.tx.us/death_row/dr_scheduled_executions.html"
+# fetch and parse the page
 doc = html.fromstring(requests.get(url).text)
-# sometimes the executions happen more quickly than they can
-# update the scheduled executions list, so we can't assume the
-# first row is always the upcoming execution
-today =  datetime(*datetime.now().timetuple()[0:3]) # whatever, too lazy to look this up
+# Get our time in central texas time; http://stackoverflow.com/a/22109768/160863
+texas_time = pytz.timezone("US/Central")
+today = texas_time.localize(datetime(*datetime.now().timetuple()[0:3])) # whatever, too lazy to look up the idiom
 for row in doc.xpath('//table/tr')[1:]:
+    # Even though this table is sorted in reverse-chronological order,
+    #  sometimes the executions happen more quickly than the updates to the
+    #  webpage, can't assume the first row is always the upcoming execution
+    #
+    # Each row looks like:
+    # | 08/12/2015 |  Info | Lopez | Daniel | 999555 | 09/15/1987 | H | 03/16/2010 | Nueces |
     col = row.cssselect('td')[0]
     exdate = datetime.strptime(col.text_content(), '%m/%d/%Y')
-    # meh, this doesn't account for timezones but whatever, too lazy to look this up
+    exdate = texas_time.localize(exdate)
     if (exdate >= today):
         print((exdate - today).days, "days")
         break
