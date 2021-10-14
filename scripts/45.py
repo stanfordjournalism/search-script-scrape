d5b7e16cbf47502cb0d245ea49fff0af648b8806
Hui (Henry) Chen <hhc879099766@gmail.com>
2021-10-14 12:12:08 -0400

    Updated: the latest US-CERT site

--- scripts/45.py
+++ scripts/45.py
@@ -1,6 +1,42 @@
 # The number of security alerts issued by US-CERT in the current year
+
 import requests
 from lxml import html
-url = 'https://www.us-cert.gov/ncas/alerts'
-doc = html.fromstring(requests.get(url).text)
-print(len(doc.cssselect('.item-list li')))
+from bs4 import BeautifulSoup
+
+def getTotalSecurityCount(year):
+
+    counter = 0
+    result = 0
+    flag = True
+
+    try:
+
+        while flag:
+
+            url = "https://www.us-cert.gov/ncas/alerts/" + year + "?page=" + str(counter)
+            page = requests.get(url)
+
+            if page.status_code == 200:
+
+                bs = BeautifulSoup(page.text, "html.parser")
+
+                r = bs.find_all("div", {"class": "views-field views-field-title"})
+                
+                if len(r) > 0:
+                    result = result + len(r)
+                    counter = counter + 1
+                else:
+                    flag = False
+
+            else:
+                flag = False
+    except:
+        print("Exception raised")
+    
+
+    return result
+
+year = input("Input current year: ")
+
+print(f"Total security alert in {year} is: {getTotalSecurityCount(year)}")
